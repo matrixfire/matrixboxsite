7ba1518a69ecfbededfa219bf07b6f074de61de5
Your Name <you@example.com>
2023-08-02 15:16:32 +0800

    82 ok

--- info_app/views.py
+++ info_app/views.py
@@ -1,9 +1,14 @@
-from django.http import Http404, HttpResponse
+import time
+
+from django.http import Http404, HttpResponse, JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
+def index2(request): # old
     return render(request, "singlepage/index.html")
+
+def index(request):
+    return render(request, "data/index.html")
 
 # The texts are much longer in reality, but have
 # been shortened here to save space
@@ -14,3 +19,23 @@ def section(request, num):
         return HttpResponse(texts[num - 1])
     else:
         raise Http404("No such section")
+    
+
+def posts(request):
+
+    # Get start and end points
+    start = int(request.GET.get("start") or 0)
+    end = int(request.GET.get("end") or (start + 9))
+
+    # Generate list of posts
+    data = []
+    for i in range(start, end + 1):
+        data.append(f"Post #{i}")
+
+    # Artificially delay speed of response
+    time.sleep(1)
+
+    # Return list of posts
+    return JsonResponse({
+        "posts": data
+    })

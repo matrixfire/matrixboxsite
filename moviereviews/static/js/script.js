d16430d408d4ff6606c326d1b5050e7166248050
Your Name <you@example.com>
2023-08-07 15:18:33 +0800

    ok 2023 8 7

--- moviereviews/static/js/script.js
+++ moviereviews/static/js/script.js
@@ -36,8 +36,27 @@ function add_post(contents) {
     // Create new post
     const post = document.createElement('div');
     post.className = 'post';
-    post.innerHTML = contents;
+    // post.innerHTML = contents;
+    post.innerHTML = `${contents} <button class="hide">Hide</button>`;
 
     // Add post to DOM
     document.querySelector('#posts').append(post);
 };
+
+// If hide button is clicked, delete the post
+document.addEventListener('click', event => {
+
+    // Find what was clicked on
+    const element = event.target;
+
+    // Check if the user clicked on a hide button
+    if (element.className === 'hide') {
+        element.parentElement.style.animationPlayState = 'running';
+        element.parentElement.addEventListener('animationend', () => {
+            element.parentElement.remove();
+        });
+
+
+    }
+    
+});
